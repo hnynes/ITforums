6cd7ff56b5c33f9ef58ebc2dca8285f0a1cb78b7
superliu <734190426@qq.com>
2018-11-16 23:30:12 +0800

    增加后台登录用户模型

---
+++ app/cms/models.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# Copyright © superliuliuliu1
+# File Name: /cms/models.py
+# Author: superliuliuliu1
+# Email: superliuliuliu1@gmail.com
+# Created: 2018-11-16 8:51:50 (CST)
+# Last Update:
+#          By:
+# Description:定义后台管理的数据库模型
+# **************************************************************************
+
+
+from .. import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# 后台管理用户模型
+class CMSUser(db.Model):
+    __tablename__ = 'cms_user'
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    username = db.Column(db.String(64), nullable = False)
+    password_hash = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(64), nullable = False, unique = True)
+    join_time = db.Column(db.DateTime, default = datetime.now)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password  #此处会调用setter修饰的函数，将password转换为hash形式并存储到数据库中
+        self.email = email
+
+    #对外以password的形式，对内以password_hash的形式
+    #property  将类的方法定义成属性的形式,其往往和setter组合使用
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    # setter作用是使其可以通过user.password = 'xxx'的形式来设置密码
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # 检验密码
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
