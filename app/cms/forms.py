398c277888da109586e1cd9427fc691e452816ea
superliu <734190426@qq.com>
2018-11-20 23:55:44 +0800

    reset password使用的js文件

--- app/cms/forms.py
+++ app/cms/forms.py
@@ -14,9 +14,16 @@
 from wtforms import Form, StringField, SubmitField, SelectField, PasswordField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp, InputRequired
 from wtforms import ValidationError
+from ..forms import BaseForm
 
 
-class LoginForm(Form):
+class LoginForm(BaseForm):
     email = StringField('Email', description='Email', validators=[Email(message='请输入正确格式的邮箱'), InputRequired(message='请输入邮箱')])
     password = PasswordField('密码', description='password', validators=[Length(8, 16, message='请输入正确格式的密码')])
     remember = IntegerField()
+
+
+class ResetPwdForm(BaseForm):
+    oldpwd = PasswordField('旧密码', validators=[DataRequired(), Length(8, 18, message='请输入正确格式的旧密码')])
+    newpwd = PasswordField('新密码', validators=[DataRequired(), Length(8, 18, message='请输入正确格式的旧密码')])
+    newpwd2 = PasswordField('新密码', validators=[DataRequired(), EqualTo("newpwd", message='确认密码必须和新密码保持一致')])
---
+++ app/static/cms/js/resetpassword.js
@@ -0,0 +1,32 @@
+
+$(function () {
+    //获取按钮
+    $("#submit").click(function (event) {
+        //因为是以aljx方式提交表单数据，因此需要禁止表单的默认提交
+        event.preventDefault();
+
+        //获取表单输入的数据
+        var ele1 = $("input[name=oldpassword]");
+        var ele2 = $("input[name=newpassword]");
+        var ele3 = $("input[name=newpassword2]");
+
+        var oldpwd = ele1.val();
+        var newpwd = ele2.val();
+        var newpwd2 = ele3.val();
+
+        myajax.post({
+            'url': 'cms/password/',
+            'data':{
+                'oldpwd': oldpwd,
+                'newpwd': newpwd,
+                'newpwd2': newpwd2
+            },
+            'success': function (data) {
+                console.log(data);
+            },
+            'fail': function (error) {
+                console.log(error);
+            }
+        });
+    });
+});
