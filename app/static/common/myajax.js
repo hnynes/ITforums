c9a2d1137313e1e86756ce7fc14e12c90fb287a9
superliu <734190426@qq.com>
2018-11-21 16:19:41 +0800

    代码规范化

--- app/static/common/myajax.js
+++ app/static/common/myajax.js
@@ -1,14 +1,20 @@
 
-// **************************************************************************
-// Copyright © superliuliuliu1
-// File Name: manage.py
-// Author: superliuliuliu1
-// Email: superliuliuliu1@gmail.com
-// Created: 2018-11-20 21:20:50 (CST)
-// Last Update:
-//        By:
-// Description:对jquery的ajax的封装
-// **************************************************************************
+/*************************************************************************
+ Copyright © superliuliuliu1
+ File Name: manage.py
+ Author: superliuliuliu1
+ Email: superliuliuliu1@gmail.com
+ Created: 2018-11-20 21:20:50 (CST)
+ Last Update:
+        By:
+ Description:对jquery的ajax的封装 包含防止CSRF攻击
+# AJAX(ajax)
+# Async JavaScript And XML
+# Async（异步）：网络请求是异步的。
+# JavaScript：JavaScript语言
+# And：并且
+# XML：JSON
+**************************************************************************/
 
 'use strict';
 var myajax = {
