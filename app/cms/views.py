28067eb66a9f6181f058ec2953fb1cb64bfe3670
superliu <734190426@qq.com>
2018-11-21 16:18:37 +0800

    增加修改密码

--- app/cms/views.py
+++ app/cms/views.py
@@ -12,12 +12,13 @@
 # **************************************************************************
 
 
-from flask import Blueprint, views, render_template, request, session, redirect, url_for, g
-from .forms import LoginForm
+from flask import Blueprint, views, render_template, request, session, redirect, url_for, g, jsonify
+from .forms import LoginForm, ResetPwdForm
 from .models import CMSUser
 from .decorators import login_required
 from config import config
 from flask_login import logout_user
+from .. import db
 
 bp = Blueprint('cms', __name__, url_prefix='/cms')
 
@@ -61,18 +62,36 @@ class LoginView(views.MethodView):
             else:#用户存在但是密码不正确
                 return self.get(message='用户名或密码有误，请重新输入或找回密码')
         else:
-            message = form.errors.popitem()[1][0] #form.errors 当表单提交出错的时候是一个列表的形式，
+            message = form.get_error()
             #利用popitem获取列表中的第一个错误信息[1]代表错误信息的value,[0]代表将错误信息以字符串的形式提取出来
             return self.get(message=message)
 
+# 重置密码的视图类
 class ResetPasswordView(views.MethodView):
     decorators = [login_required]
     def get(self, message=None):
         return render_template('cms/cms_reset_password.html')
 
     def post(self):
-        pass
-
+        form = ResetPwdForm(request.form)
+        if form.validate():
+            oldpwd = form.oldpwd.data
+            newpwd = form.newpwd.data
+            # 因为在修改密码时，用户已经登录，所以Flask上下文全局对象g存储的有其相关信息
+            user = g.cms_user
+            # 如果原密码正确
+            if user.check_password(oldpwd):
+                user.password = newpwd
+                db.session.commit()
+                # 因为使用的是ajax所以需要返回json数据
+                # 定义的返回码200代表成功，无返回消息
+                # 返回码为400 代表失败，
+                return jsonify({"code": 200, "message": ""})
+            else:
+                return jsonify({"code": 400, "message": "原密码错误"})
+        else:
+            message = form.get_error()
+            return jsonify({"code": 400, "message": message})
 
 
 @bp.route('/logout')
@@ -83,5 +102,5 @@ def logout():
 
 
 
-bp.add_url_rule('/login', view_func=LoginView.as_view('login'))
-bp.add_url_rule('/password', view_func=ResetPasswordView.as_view('password'))
+bp.add_url_rule('/login/', view_func=LoginView.as_view('login'))
+bp.add_url_rule('/password/', view_func=ResetPasswordView.as_view('password'))
