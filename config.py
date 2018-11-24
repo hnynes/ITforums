376180123c9ee5c7063b0efd0cb68b38ad260f8d
superliu <734190426@qq.com>
2018-11-24 10:07:17 +0800

    修改邮箱邮件html文件和txt文本消息

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ class Config:
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     #用户名和密码采取导入环境变量的形式防止密码泄露
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # '734190426@qq.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') # 'irptfbwxnrvkbcig'
+    MAIL_USERNAME = '734190426@qq.com'
+    MAIL_PASSWORD = 'irptfbwxnrvkbcig'
     FLASKY_MAIL_SENDER = '734190426@qq.com'  # this is the sender email
     FLASKY_MAIL_SUBJECT_PREFIX = '[FORUMIT]' # the part of the subject of the mail
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
