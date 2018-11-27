1b7038cca50e228c44148ffd46625f646864c68a
superliu <734190426@qq.com>
2018-11-27 18:48:07 +0800

    pip install pillow

--- utils/tools.py
+++ utils/tools.py
@@ -1,1 +1,14 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# Copyright © superliuliuliu1
+# File Name: tools.py
+# Author: superliuliuliu1
+# Email: superliuliuliu1@gmail.com
+# Created: 2018-11-27 18:42:50 (CST)
+# Last Update:
+#          By:
+# Description:生成图形验证码
+# **************************************************************************
+import random
+import string
