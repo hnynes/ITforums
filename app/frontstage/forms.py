4d60a4ac6331a54a3d0f46f3da79e5291286e182
superliu <734190426@qq.com>
2018-11-29 19:44:55 +0800

    注册页面所需的表单

---
+++ app/frontstage/forms.py
@@ -0,0 +1,29 @@
+from wtforms import Form, StringField, SubmitField, SelectField, PasswordField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp, InputRequired
+from wtforms import ValidationError
+from ..forms import BaseForm
+from utils import mycache
+from flask import g
+
+class RegisterForm(BaseForm):
+    telephone = StringField(validators=[Regexp(r'1[345789]\d{9}')])
+    # 短信验证码
+    messagecode = StringField(validators=[Regexp(r'\d{6}')])
+    username = StringField(validators=[Regexp(r'.{2,20}', message='请输入正确格式的用户名！')])
+    password1 = StringField(validators=[Regexp(r'[0-9a-zA-Z_\.]{6,20}', message='请输入正确格式的密码！')])
+    password2 = StringField(validators=[EqualTo("password1", message='两次输入的密码不一致！')])
+    # 图形验证码
+    picturecode = StringField(validators=[Regexp(r'\w{4}', message="验证码格式不正确！")])
+
+    def validate_messagecode(self, field):
+        totest = field.data
+        telephone = self.telephone.data
+        messagecode = mycache.get(telephone)
+        if not messagecode or messagecode != totest:
+            raise ValidationError("短信验证码错误！")
+
+    def validate_picturecode(self, field):
+        totest = field.data
+        picturecode = mycache.get(totest)
+        if not picturecode or totest.lower() != picturecode.lower():
+            raise ValidationError("图形验证码错误！")
