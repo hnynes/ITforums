820dddbcc532cdee218dd61b7ca1e695847226e8
superliu <734190426@qq.com>
2018-11-29 19:46:32 +0800

    一个手机账号只能注册一个账号，因此在注册时发送验证码的过程之中会首先检测该手机号在数据库是否存在

--- app/common/views.py
+++ app/common/views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request, make_response
 from utils.picture import Captcha
 from io import BytesIO
-from utils import aliyunmessage, restful
+from utils import aliyunmessage, restful, mycache
 import string
 import random
+from .forms import SendmessageForm
+from ..frontstage.models import FrontUser
 
 bp = Blueprint('common', __name__, url_prefix='/common')
 
@@ -19,19 +21,27 @@ def growpicture():
     out.seek(0)
     p = make_response(out.read())
     p.content_type = "image/png"
+    # 存储图形验证码内容到memached中
+    mycache.set(text.lower(), text.lower())
     return p
 
-@bp.route('/sendmessage/')
+# 防止受到攻击 对调用短信接口进行加密操作  这次采用post的方式来发送验证码
+# 加密操作的原理 是让前台在向后端服务器发送手机号码数据时，要加上其他的信息 比如时间戳等(当然更安全的是采取一些加密机制防止爬虫) 只有这些信息检验正确时才会调用短信接口去执行相应的操作
+@bp.route('/sendmessage/', methods=['POST'])
 def sendmessage():
-    phonenumber = request.args.get('telephone')
-    if not phonenumber:
-        # 如果没有输入手机号码就点击发送验证码 则提示输入手机号
-        return restful.args_error("请输入手机号码")
-    else:
-        # 验证码为随机生成的6位数字
+    form = SendmessageForm(request.form)
+    if form.validate():
         source = list(map(lambda x: str(x), range(0, 10)))
         code = "".join(random.sample(source, 6))
+        phonenumber = form.telephone.data
+        testphone = FrontUser.query.filter_by(telephone=phonenumber).first()
+        if testphone:
+            return restful.args_error("此手机号已注册过账号，请检查或者去登录！")
         if aliyunmessage.send_sms(phone_numbers = phonenumber, code = code):
+            # 将验证码存储到memached中
+            mycache.set(phonenumber, code)
             return restful.success()
         else:
             return restful.args_error("短信验证码发送失败")
+    else:
+        return restful.args_error("参数错误")
