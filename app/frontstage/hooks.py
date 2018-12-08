482a0ab93a4e37bca1664cd44dd2291b4e5f7ee3
superliu <734190426@qq.com>
2018-12-08 17:05:36 +0800

    增加前台的钩子函数

---
+++ app/frontstage/hooks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# Copyright © superliuliuliu1
+# File Name: hooks.py
+# Author: superliuliuliu1
+# Email: superliuliuliu1@gmail.com
+# Created: 2018-12-8 15:49:32 (CST)
+# Last Update:
+#          By:
+# Description:前台所需的钩子函数
+# **************************************************************************
+
+from .views import bp
+from flask import g, session
+from .models import FrontUser
+from config import config
+
+@bp.before_request
+def before_request():
+    if config['development'].FRONTUSERID in session:
+        userid = session.get(config['development'].FRONTUSERID)
+        user = FrontUser.query.get(userid)
+        if user:
+            g.front_user = user
